43d164eef15a1b0e6f5d086ca4f7f57955265854
Nakakandebr <bridgetnakakandea@gmail.com>
2023-04-14 22:18:04 +0300

    commit

--- assignment2_python/assignment2.py
+++ assignment2_python/assignment2.py
@@ -1,4 +1,4 @@
-
+# A function named concatenate_args that takes any number of string arguments in positional arguments format and concatenates them into a single string
 def concatenate_args(*args):
     answer = ""
     for arg in args:
